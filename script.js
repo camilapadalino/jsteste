52492494e43518c7db09734c22eb25d056a77484
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-03 11:21:57 -0300

    jsteste

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let opcao = parseInt(prompt('Digite uma opção: '))
+/*let opcao = parseInt(prompt('Digite uma opção: '))
 
 switch(opcao){
     case 1: 
@@ -14,4 +14,30 @@ switch(opcao){
     default : 
         document.write('Quarta Opção');
      break;
+}
+*/
+/*
+let valor = 8
+document.write(valor)
+document.write("<br><br>")
+valor = valor + 3
+document.write(valor)
+document.write("<br><br>")
+
+let valor1 = 6
+document.write(valor1)
+document.write("<br><br>")
+valor1 = valor1 - 2
+document.write(valor1)
+
+for (let i=0; i < 10; i++){
+    document.write("Agora i vale" +i);
+    document.write("<br><br>")
+    
+}
+*/
+let carros = ["Gol", "Fusca", "Hb20", "Corsa"]
+for(let i=0; i < carros.length; i++){
+    document.write(carros[i]);
+    document.write('<br><br>')
 }
